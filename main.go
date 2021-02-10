ffae00719ddb2827b7bc356e04411825d20c8c52
Roman Dibikhin <dibikhin@gmail.com>
2021-02-10 23:46:01 +0300

    filtered input

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -49,35 +48,39 @@ func main() {
 
 	// Game loop
 	for {
+		if !hasEmpty(board) {
+			break
+		}
 		fmt.Print("Player 1, your turn: ")
-
-		scanner.Scan()
-		p1turn := scanner.Text()
-
-		if p1turn != "" {
-			num, err := strconv.Atoi(p1turn)
-			if err != nil {
-				panic(err)
-			}
-			row, col := pos(num)
-			board[row][col] = "X"
+		p1turn := getInput(scanner)
+		for !isTurn(p1turn) {
 			print(board)
+			fmt.Print("Player 1, your turn: ")
+			p1turn = getInput(scanner)
+		}
+		row, col := pos(p1turn)
+		board[row][col] = "X"
+		print(board)
+
+		if !hasEmpty(board) {
+			break
 		}
 		fmt.Print("Player 2, your turn: ")
-
-		scanner.Scan()
-		p2turn := scanner.Text()
-
-		if p2turn != "" {
-			num, err := strconv.Atoi(p2turn)
-			if err != nil {
-				panic(err)
-			}
-			row, col := pos(num)
-			board[row][col] = "O"
+		p2turn := getInput(scanner)
+		for !isTurn(p2turn) {
 			print(board)
+			fmt.Print("Player 2, your turn: ")
+			p2turn = getInput(scanner)
 		}
+		row, col = pos(p2turn)
+		board[row][col] = "O"
+		print(board)
 	}
+}
+
+func getInput(s *bufio.Scanner) string {
+	s.Scan()
+	return s.Text()
 }
 
 func print(b grid) {
@@ -91,20 +94,41 @@ func print(b grid) {
 	fmt.Println()
 }
 
-func pos(num int) (row, col int) {
-	m := map[int]struct {
+func hasEmpty(b grid) bool {
+	for _, r := range b {
+		for _, v := range r {
+			if v == "_" {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func isTurn(s string) bool {
+	st := strings.TrimSpace(s)
+	for _, v := range strings.Split("123456789", "") {
+		if st == v {
+			return true
+		}
+	}
+	return false
+}
+
+func pos(key string) (row, col int) {
+	m := map[string]struct {
 		row, col int
 	}{
-		1: {0, 0},
-		2: {0, 1},
-		3: {0, 2},
-		4: {1, 0},
-		5: {1, 1},
-		6: {1, 2},
-		7: {2, 0},
-		8: {2, 1},
-		9: {2, 2},
+		"1": {0, 0},
+		"2": {0, 1},
+		"3": {0, 2},
+		"4": {1, 0},
+		"5": {1, 1},
+		"6": {1, 2},
+		"7": {2, 0},
+		"8": {2, 1},
+		"9": {2, 2},
 	}
-	pos := m[num]
+	pos := m[key]
 	return pos.row, pos.col
 }
